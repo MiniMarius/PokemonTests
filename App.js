c757816661916523cad3b26c5cbca332a1d557a9
Marius Seffer <marius.seffer@combitech.com>
2023-08-15 13:52:12 +0200

    pokemon example created

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+  Image,
+  ImageBackground,
+} from "react-native";
 
 export default function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+      const json = await response.json();
+
+      const detailsPromises = json.results.map(async (pokemon) => {
+        const detailsResponse = await fetch(pokemon.url);
+        const detailsJson = await detailsResponse.json();
+        return detailsJson;
+      });
+
+      const detailsData = await Promise.all(detailsPromises);
+
+      setPokemonData(detailsData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./assets/background.jpg")}
+        resizeMode="cover"
+        style={styles.bgimage}
+      ></ImageBackground>
+      <View style={styles.header}>
+        <Image
+          style={styles.image}
+          source={require("./assets/pokemonlogo.png")}
+        />
+      </View>
+      <ScrollView style={styles.gridContainer}>
+        {isLoading ? (
+          <ActivityIndicator style={styles.loadingspinner}></ActivityIndicator>
+        ) : (
+          <View style={styles.grid}>
+            {pokemonData.map((pokemon, index) => {
+              return (
+                <View key={index} style={styles.box}>
+                  <Image
+                    style={styles.pokeImage}
+                    source={{
+                      uri: pokemon.sprites.front_default,
+                    }}
+                  />
+                  <Text style={styles.text}>{pokemon.name}</Text>
+                </View>
+              );
+            })}
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +78,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    padding: 50,
+  },
+  gridContainer: {
+    width: "100%",
+  },
+  image: {
+    width: 150,
+    height: 60,
+  },
+  bgimage: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    opacity: 0.5,
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  box: {
+    height: 150,
+    width: 150,
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 15,
+    margin: 10,
+  },
+  pokeImage: {
+    height: 100,
+    width: 100,
+  },
+  text: {
+    fontSize: 18,
   },
 });
